4fe8087dd935f70ce9d1e43ca84b7c536c89c8e7
Cezary <cezarygraban@gmail.com>
2020-01-19 20:46:36 +0100

    Labirynth layout generated and stored in simplest form (without generated path yet)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
 
 using namespace std;
+
+typedef vector<char> Row;
+typedef vector<Row> Labirynth;
+
+enum RowType {FIRST, STANDARD, LAST, NO_ROW};
 
 void getUserInput(int *height, int *width);
 bool checkInput(int height, int width);
 
+Labirynth generateLabirynth(int height, int width);
+void showLabirynth(Labirynth &labirynth);
+
+RowType getRowType(int currentIndex, int maxIndex);
+void setRow(Row &row, RowType rowType);
+
+int getRandomIntFromRange(int rangeStart, int rangeEnd);
+
 int main()
 {
+    srand(time(NULL));
+
     int h,w;
     getUserInput(&h, &w);
 
     cout << "Wysokosc to " << h << ", a szerokosc to: " << w << endl;
+
+    Labirynth labirynth = generateLabirynth(h, w);
+
+    showLabirynth(labirynth);
+
 
     return 0;
 }
@@ -33,4 +56,62 @@ void getUserInput(int *height, int *width) {
 
 bool checkInput(int height, int width) {
     return height > 0 && width > 0;
+}
+
+Labirynth generateLabirynth(int height, int width) {
+
+    Labirynth labirynth(height, Row(width));
+
+    RowType rowType = NO_ROW;
+
+    for(int i=0; i<height; i++) {
+        rowType = getRowType(i, height - 1);
+
+        setRow(labirynth[i], rowType);
+    }
+
+    return labirynth;
+}
+
+RowType getRowType(int currentIndex, int maxIndex) {
+    if (currentIndex == 0)
+        return FIRST;
+    else if (currentIndex == maxIndex)
+        return LAST;
+    else
+        return STANDARD;
+}
+
+void setRow(Row &row, RowType rowType) {
+    for(int i=0; i<row.size(); i++) {
+        switch(rowType) {
+            case FIRST : {
+                row[i] = 'S';
+                break;
+            }
+            case STANDARD : {
+                row[i] = 'm';
+                break;
+            }
+            case LAST : {
+                row[i] = 'L';
+                break;
+            }
+        }
+    }
+}
+
+void showLabirynth(Labirynth &labirynth) {
+    cout << endl;
+
+    for (Row row : labirynth) {
+        for(int i=0; i<row.size(); i++){
+            cout << row[i];
+        }
+        cout << endl;
+    }
+}
+
+int getRandomIntFromRange(int rangeStart, int rangeEnd) {
+    return rand() % ((rangeEnd - rangeStart) + 1) + rangeStart;
 }
